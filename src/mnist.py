a42ef97db6ad03bdf3d08166be9f34b36a7f579e
Pedro Manuel Gómez-Portiollo <gomezportillo@correo.ugr.es>
2018-11-23 18:44:22 +0100

    uploading version 2 (better test accuracy, worse train accuracy)

--- src/mnist.py
+++ src/mnist.py
@@ -16,28 +16,39 @@ from mnist_functions import *
 
 # Getting unique values from labels, sorting them and storing
 class_names = list(set(train_labels))
+num_classes = len(class_names)
+input_shape = (28, 28, 1) # 28x28 pixels, no z coordinate
+
+# Printing how manny different numbers are in each set
+print_unique_numbers('train', train_labels)
+print_unique_numbers('test', test_labels)
 
 # Prepocessing
 ## Normalising datasets before feeding to the neural network
 train_images = train_images / 255.0
 test_images = test_images / 255.0
 
-print_unique_numbers('train', train_labels)
-print_unique_numbers('test', test_labels)
+train_images = train_images.reshape(train_images.shape[0], 28, 28, 1)
+train_images = train_images.astype('float32')
+
+test_images = test_images.reshape(test_images.shape[0], 28, 28, 1)
+test_images = test_images.astype('float32')
+
+## convert class vectors to binary class matrices
+train_labels = keras.utils.to_categorical(train_labels, num_classes)
+test_labels = keras.utils.to_categorical(test_labels, num_classes)
 
 # Building the model
-LAYER_1 = keras.layers.Flatten(input_shape=(28, 28))
-LAYER_2 = keras.layers.Dense(128, activation=tf.nn.relu)
-LAYER_3 = keras.layers.Dense(10, activation=tf.nn.softmax)
-
 model = keras.Sequential()
-model.add( LAYER_1 )
-model.add( LAYER_2 )
-model.add( LAYER_3 )
+model.add(keras.layers.Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=input_shape))
+model.add(keras.layers.MaxPooling2D(pool_size=(2, 2)))
+model.add(keras.layers.Dropout(0.25))
+model.add(keras.layers.Flatten())
+model.add(keras.layers.Dense(num_classes, activation='softmax'))
 
 # Compiling the model
-model.compile(optimizer=tf.train.AdamOptimizer(),
-              loss='sparse_categorical_crossentropy',
+model.compile(optimizer=keras.optimizers.Adam(),
+              loss=keras.losses.categorical_crossentropy,
               metrics=['accuracy'])
 
 # Train the model with the train images
@@ -48,12 +59,11 @@ train_time_str = train_model(model, train_images, train_labels, EPOCHS)
 # Evaluate the accuracy of the model with the train images
 train_loss_str, train_acc_str = evaluate_model('Train', model, train_images, train_labels)
 
-
 # Evaluate the accuracy of the model with the test images
 test_loss_str, test_acc_str = evaluate_model('Test', model, test_images, test_labels)
 
 # Save results
-VERSION = 1
+VERSION = 2
 save_results(VERSION, train_time_str, train_loss_str, train_acc_str, test_loss_str, test_acc_str)
 
 # Predicting the labels of all train images
